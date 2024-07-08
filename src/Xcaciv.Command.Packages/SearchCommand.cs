e6ca77659e759bfc7e1cfc607dbd77fa8165fd6b
ac4 <ac-4@users.noreply.github.com>
2024-07-08 13:55:08 -0600

    initial package manager poc

---
+++ src/Xcaciv.Command.Packages/SearchCommand.cs
@@ -0,0 +1,52 @@
+﻿using NuGet.Common;
+using NuGet.Configuration;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
+using Xcaciv.Command.Core;
+using Xcaciv.Command.Interface;
+using Xcaciv.Command.Interface.Attributes;
+
+namespace Xcaciv.Command.Packages
+{
+    [CommandRoot("Package", "Package commands")]
+    [CommandRegister("Search", "install a package")]
+    [CommandParameterOrdered("search string", "String associated to the desired package.", IsRequired = true)]
+    public class SearchCommand : AbstractCommand
+    {
+        public override string HandleExecution(string[] parameters, IEnvironmentContext env)
+        {
+            // return "Not searching " + String.Join(',', parameters);
+            var packageSourceUrl = env.GetValue("PackageSourceUrl");
+            return String.Join("\n", this.SearchNugetFeed(String.Join(',', parameters), packageSourceUrl));
+        }
+
+        public override string HandlePipedChunk(string pipedChunk, string[] parameters, IEnvironmentContext env)
+        {
+            return $"Unsupported search method for {pipedChunk} " + String.Join(',', parameters);
+        }
+
+        protected async Task<List<string>> SearchNugetFeed(string searchTerm, string packageSourceUrl)
+        {
+            List<string> packageNames = new List<string>();
+
+            // "https://api.nuget.org/v3/index.json"
+            var nugetSource = new PackageSource(packageSourceUrl);
+            SourceRepository repository = Repository.Factory.GetCoreV3(nugetSource);
+
+            PackageSearchResource resource = await repository.GetResourceAsync<PackageSearchResource>();
+
+            using (var sourceCacheContext = new SourceCacheContext())
+            {
+                SearchFilter searchFilter = new SearchFilter(includePrerelease: false);
+                IEnumerable<IPackageSearchMetadata> packages = await resource.SearchAsync(searchTerm, searchFilter, skip: 0, take: 100, NullLogger.Instance, CancellationToken.None);
+
+                foreach (var package in packages)
+                {
+                    packageNames.Add(package.Identity.Id);
+                }
+            }
+
+            return packageNames;
+        }
+    }
+}
---
+++ src/Xcaciv.Cupcake.Core/ConsoleContext.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xcaciv.Command;
+using Xcaciv.Command.Core;
+using Xcaciv.Command.Interface;
+
+namespace Xcaciv.Cupcake.Core
+{
+    internal class ConsoleContext(string name = "ConsoleIo", string[]? parameters = default, Guid? parentId = default, bool verbose = true) : AbstractTextIo(name, [.. parameters], parentId)
+    {
+        /// <summary>
+        /// template for progress text output
+        /// 0 - context name
+        /// 1 - int progress
+        /// </summary>
+        public string ProgressTemplate { get; set; } = "{0} progress {1}%";
+
+        // format color defaults
+        public ConsoleColor ForegroundColor { get; set; } = ConsoleColor.Blue;
+        public ConsoleColor BackgroundColor { get; set; } = ConsoleColor.Black;
+
+        public ConsoleColor StatusForegroundColor { get; set; } = ConsoleColor.Yellow;
+        public ConsoleColor StatusBackgroundColor { get; set; } = ConsoleColor.DarkBlue;
+
+        public ConsoleColor PromptForegroundColor { get; set; } = ConsoleColor.Green;
+        public ConsoleColor PromptBackgroundColor { get; set; } = ConsoleColor.Black;
+        public new bool Verbose { get; set; } = verbose;
+
+        /// <summary>
+        /// <see cref="IIoContext"/>
+        /// </summary>
+        /// <param name="childParameters"></param>
+        /// <returns></returns>
+        public override Task<IIoContext> GetChild(string[]? childParameters = null)
+        {
+            var child = new ConsoleContext(this.Name + "Child", childParameters, Id);
+
+            if (this.HasPipedInput && this.inputPipe != null) child.SetInputPipe(this.inputPipe);
+
+            if (this.outputPipe != null) child.SetOutputPipe(this.outputPipe);
+
+            return Task.FromResult<IIoContext>(child);
+        }
+        /// <summary>
+        /// <see cref="AbstractTextIo"/>
+        /// </summary>
+        /// <param name="chunk"></param>
+        /// <returns></returns>
+        public override Task HandleOutputChunk(string chunk)
+        {
+            Console.ForegroundColor = ForegroundColor;
+            Console.BackgroundColor = BackgroundColor;
+            Console.WriteLine(chunk);
+            Console.ResetColor();
+            return Task.CompletedTask;
+        }
+        /// <summary>
+        /// get input from user
+        /// </summary>
+        /// <param name="prompt"></param>
+        /// <returns></returns>
+        public override Task<string> PromptForCommand(string prompt)
+        {
+            Console.ForegroundColor = PromptForegroundColor;
+            Console.BackgroundColor = PromptBackgroundColor;
+            Console.Write(prompt);
+            return Task.FromResult(Console.ReadLine() ?? string.Empty);
+        }
+        /// <summary>
+        /// do some progress math, print a progress message and return the int progress
+        /// </summary>
+        /// <param name="total"></param>
+        /// <param name="step"></param>
+        /// <returns></returns>
+        public override Task<int> SetProgress(int total, int step)
+        {
+            int progress = total / step;
+            SetStatusMessage(string.Format(ProgressTemplate, this.Name, progress));
+            return Task.FromResult(progress);
+        }
+        /// <summary>
+        /// output status message to console
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public override Task SetStatusMessage(string message)
+        {
+            if (Verbose == false)
+            {
+                Debug.WriteLine(message);
+                return Task.CompletedTask;
+            }
+
+            Console.ForegroundColor = StatusForegroundColor;
+            Console.BackgroundColor = StatusBackgroundColor;
+            Console.WriteLine(message);
+            Console.ResetColor();
+            return Task.CompletedTask;
+        }
+    }
+}
